bd1bb22a04c95fdc62f44c348c1e5432383742b9
gabrielmpdev <gabrielmp.dev@gmail.com>
2023-09-03 15:10:14 -0300

    feat(ToDoList): init app

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import TodoTask from "./components/TodoTask";
+
+import './styles/styles.css'
+
+function App() {
+
+	return (
+		<div className="App">
+
+			<header>
+
+				<h2>Lists</h2>
+
+				<input
+					type="text" autoComplete="off" 
+					placeholder="Escrever task..." 
+					name="task"
+					className="input"
+				/>
+
+				<button type="submit" className="btn-header">Adicionar Task</button>
+			</header>
+			
+			<div className="line"></div>
+
+			<TodoTask />
+			
+		</div>
+	);
+}
+
+export default App;
